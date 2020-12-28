db5c900d57e698fd9cd1236094b2b92d1032ed12
kindeme <fkounoudji@gmail.com>
2020-12-27 23:33:15 -0600

    variales and operator

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<!-- <form method="get">
+<input type="text" name ="user">
+<button> Save</button>
+</form> -->
+    <?php
+// echo "hello world";
+// $usersName = "Manu";
+// echo " $usersName";
+// $a = 10;
+// $b = 15;
+// $c = $a + $b ;
+// echo "$c";
+
+$name = $_GET['user'];
+
+// echo  $name. ' Can play cricket'; 
+
+$array =['Senyo','Flore','Hillary','Tiffany','Ryan'];
+//echo $array;
+// print_r($array);
+// echo "<br>";
+// print_r(array_flip($array)) ;
+$amountA = 10;
+$amountB = 20;
+echo $amountA > $amountB;
+
+
+?>
+</body>
+</html>
+
